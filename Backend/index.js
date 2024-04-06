9aaf9a0c369abc0a335e1d698cbb4c6b862fc34e
Georges <gouweijan@gmail.com>
2024-04-06 12:05:47 -0400

    setup backend login fuctionality

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { connect } from './Database/MongoDBServer.js';
+import { UserData } from "./Database/models/UserData.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,4 +9,26 @@ app.get("/", (request, response) => {
     response.send({ worked: true });
 });
 
+app.get('/api/login', async (req, res) => {
+    let responseData;
+    let result = await UserData.findOne({username: req.query.name});
+
+    if(result == null){
+        responseData = {result: 'FAILURE', message: 'Failed to login, username could not be found'};
+    }
+    else{
+        result = await UserData.find({username: req.query.name, password: req.query.pass});
+        
+        if(result.length == 0){
+            responseData = {result: 'FAILURE', message: 'Failed to login, password is invalid'};
+        }
+        else{
+            responseData = {result: 'SUCCESS', account_info: result[0]._id};
+        }
+    }
+
+    res.json(responseData);
+  });
+
 app.listen(port, () => console.log(`App is running on port ${port}`));
+connect();
