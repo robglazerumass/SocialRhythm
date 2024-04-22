963138be68f2d77ca19f133d9aefa2c6928c374e
Anh Le <lemanhstudy4111@gmail.com>
2024-04-22 09:40:05 -0400

    menu bar + feed (mock data)

--- Frontend/src/components/FeedImage.tsx
+++ Frontend/src/components/FeedImage.tsx
@@ -1,19 +1,22 @@
-import spotifyLogo from "../assets/spotify_logo.png"
+import spotifyLogo from "../assets/spotify_logo.png";
 
 export default function FeedImage() {
-    const imageUrl = "https://images.pexels.com/photos/17731677/pexels-photo-17731677/free-photo-of-a-red-parachute-flying-through-the-air-in-a-blue-sky.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+	const imageUrl =
+		"https://images.pexels.com/photos/17731677/pexels-photo-17731677/free-photo-of-a-red-parachute-flying-through-the-air-in-a-blue-sky.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load";
 	return (
-			<div className="image-view relative w-3/5 -z-10">
-				<img
-					src={imageUrl}
-					className="post-image aspect-square"
-				/>
-                <a className="spotify-link absolute bottom-5 right-5" href="https://www.spotify.com">
-                    <button className="spotify-btn bg-[#191414] rounded-full flex items-center justify-center gap-3 text-[#1DB954] border-[#1DB954] h-10 w-10 lg:w-auto">
-                        <img src={spotifyLogo} className="max-h-6 max-w-6 inline bg-[#191414] rounded-full"/>
-                        <p className="hidden lg:inline">Open in spotify</p>
-                    </button>
-                </a>
-			</div>
+		<div className="image-view relative w-3/5 -z-10">
+			<img src={imageUrl} className="post-image aspect-square" />
+			<a
+				className="spotify-link absolute bottom-5 right-5"
+				href="https://www.spotify.com">
+				<button className="spotify-btn bg-[#191414] rounded-full flex items-center justify-center gap-3 text-[#1DB954] border-[#1DB954] h-10 w-10 lg:w-auto">
+					<img
+						src={spotifyLogo}
+						className="max-h-6 max-w-6 inline bg-[#191414] rounded-full"
+					/>
+					<p className="hidden lg:inline">Open in spotify</p>
+				</button>
+			</a>
+		</div>
 	);
 }
--- Frontend/src/page/Feed.tsx
+++ Frontend/src/page/Feed.tsx
@@ -2,22 +2,80 @@ import MenuBar from "../components/menubar";
 import FeedImage from "../components/FeedImage";
 import Interaction from "../components/Interaction";
 import { Bars3Icon } from "@heroicons/react/16/solid";
+import MenuBar2 from "../components/MenuBar2";
+import postMockData from "../mockData/postMockData";
 
-export default function Feed() {
+function Post({
+	username,
+	title,
+	description,
+	img,
+}: {
+	username: string;
+	title: string;
+	description: string;
+	img: string;
+}) {
 	return (
 		<div>
+			<div className="top-half flex flex-col justify-center gap-5 px-40 py-5">
+				<div className="flex flex-row gap-4">
+					<div className="avatar">
+						<div className="w-20 rounded">
+							<img src={img} />
+						</div>
+					</div>
+					<div className="min-h-full flex flex-col justify-center text-xl font-bold">
+						<p>{username}</p>
+					</div>
+				</div>
+				<div className="flex flex-col gap-4">
+					<p className="font-extrabold text-3xl">{title}</p>
+					<p>{description}</p>
+				</div>
+			</div>
+			<div className="temp-feed-view h-full overflow-y-scroll w-full flex flex-col items-center box-border">
+				<FeedImage />
+				<Interaction />
+			</div>
+		</div>
+	);
+}
+
+export default function Feed() {
+	const defaultUsername = "alice123";
+	const defaultTitle = "New Album!";
+	const defaultImg =
+		"https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg";
+	const defaultDesc =
+		"Excited for the new album release from my favorite artist!";
+	return (
+		<div className="flex flex-row min-h-screen min-w-screen">
+			<MenuBar2 />
 			{/* <MenuBar /> */}
 			{/* Add other components like title and description into the div below*/}
-			<MenuBar />
+			{/* <MenuBar />
 			<div className="menu-icon fixed top-4 left-4">
 				<label htmlFor="my-drawer-2" className="btn btn-primary drawer-button">
 					<Bars3Icon className="h-6" />
 					Menu
 				</label>
-			</div>
-			<div className="temp-feed-view h-full overflow-y-scroll w-full flex flex-col items-center box-border">
-				<FeedImage />
-				<Interaction />
+			</div> */}
+			<div>
+				{postMockData.map((post) => (
+					<Post
+						username={post.username}
+						title={post.title}
+						description={post.description}
+						img={post.image_url}
+					/>
+				))}
+				<Post
+					username={defaultUsername}
+					title={defaultTitle}
+					description={defaultDesc}
+					img={defaultImg}
+				/>
 			</div>
 		</div>
 	);
