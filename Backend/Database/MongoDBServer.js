74a9a719ffef830d868524fa0c3463bb266e4f2c
cabouezzi <chanielezzi@gmail.com>
2024-05-12 22:23:56 -0400

    Configured to use test database when running jest

--- Backend/Database/MongoDBServer.js
+++ Backend/Database/MongoDBServer.js
@@ -3,13 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const db = process.env.JEST_WORKER_ID === undefined ? "SocialRhythmDB" : "SocialRhythmDBTest"
 const password = process.env.DB_PASSWORD;
-const uri = 'mongodb+srv://gouweijan:' + password + '@socialrhythmdb.fupvhxa.mongodb.net/SocialRhythmnDB?retryWrites=true&w=majority&appName=SocialRhythmDB';
+const uri = 'mongodb+srv://gouweijan:' + password + `@socialrhythmdb.fupvhxa.mongodb.net/${db}?retryWrites=true&w=majority&appName=SocialRhythmDBTest`;
 
 async function connect() {
     try {
         await mongoose.connect(uri);
-        console.log("Connected to MongoDB");
+        console.log(`Connected to MongoDB using ${db}`);
 
     } catch (error) {
         console.error(error);
