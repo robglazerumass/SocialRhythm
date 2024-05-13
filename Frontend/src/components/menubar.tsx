07aa3d4d339be3c56a7e60cda2082bf3519e3185
jiexy <ifechumek@gmail.com>
2024-05-13 00:34:43 -0400

    connected search to the backend

--- Frontend/src/components/menubar.tsx
+++ Frontend/src/components/menubar.tsx
@@ -1,37 +1,51 @@
 import { ReactNode } from "react";
-import { MagnifyingGlassIcon, UserGroupIcon, UserIcon } from "@heroicons/react/24/outline";
+import {
+	HomeIcon,
+	UserCircleIcon,
+} from "@heroicons/react/24/outline";
+import { useLocation } from "react-router-dom";
+import SearchModal from "./SearchModal";
 
-function MenuItems({ icon, name }: { icon: ReactNode; name: string }) {
+interface MenuBarProps {
+	icon: ReactNode;
+	name: string;
+	linkTo: string;
+}
+
+function MenuItems({ icon, name, linkTo}: MenuBarProps) {
 	return (
-		<li>
-			<a className="flex justify-center gap-2 text-white flex-col p-0 items-center">
+		<button className="m-0 p-0 bg-transparent">
+			<a
+				className="flex justify-center gap-2 text-white flex-col p-0 items-center"
+				href={`../${linkTo}`}
+			>
 				{icon}
 				{name}
 			</a>
-		</li>
+		</button>
 	);
 }
 
 export default function MenuBar() {
+	const { state } = useLocation();
+	const { username } = state;
 	return (
 		<div className="fixed z-10 left-10 h-screen flex flex-col justify-center w-1/12">
-			<ul className="menu h-2/5 flex bg-primary bg-opacity-30 justify-center rounded-full w-8/12">
+			<ul className="menu h-2/5 flex bg-primary bg-opacity-30 justify-center rounded-full w-8/12 shadow-lg shadow-black">
 				<div className="h-4/5 flex flex-col justify-between w-full">
-				<MenuItems
-						icon={ <MagnifyingGlassIcon className="w-6 h-6"/> }
-						name="Search"
+				<SearchModal/>
+					<MenuItems
+						icon={<HomeIcon className="w-6 h-6" />}
+						name="Feed"
+						linkTo="feed"
 					/>
 					<MenuItems
-						icon={ <UserGroupIcon className="h-6 w-6" />}
-						name="Friends"
-					/>
-					<MenuItems
-						icon={ <UserIcon className="h-6 w-6"/>}
-						name="Trending"
+						icon={<UserCircleIcon className="h-6 w-6" />}
+						name="Profile"
+						linkTo={`/user/${username}`}
 					/>
 				</div>
 			</ul>
 		</div>
 	);
 }
-
