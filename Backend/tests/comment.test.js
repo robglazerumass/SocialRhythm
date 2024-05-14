145ac4bcc188866da57db0b7c52a4f37d02fb89c
RyanTavol <121977925+RyanTavol@users.noreply.github.com>
2024-05-14 18:23:02 -0400

    Comment testing (#53)
    * account.js includes helper functions for login and signup
    
    * posts.js includes helper functions to abstract the functionality of the apis
    
    * users.js includes helper functions to abstract functionality from api endpoints
    
    * cleaned up code a bit
    
    * Moved files into src directory
    
    * Added jsdoc dependency and npm command for building docs
    
    * Fixed bug in spotify
    
    * Setup jest and spotify test suite
    
    * Figured out how to test API integration.
    
    * Configured to use test database when running jest
    
    * Corrected typo (technically I was right. existing db has a typo)
    
    * set up framework. These tests aren't final
    
    * added several tests and fixed code in feed
    
    * fixed another documentation error
    
    * deleted uneccesary print
    
    * added testing for profile
    
    * added testing for createComment
    
    * added testing for getComment. Added small bug fixes too
    
    * added a few more misc tests
    
    ---------
    
    Co-authored-by: cabouezzi <chanielezzi@gmail.com>

---
+++ Backend/tests/comment.test.js
@@ -0,0 +1,165 @@
+import { getComments, createComment } from "../src/posts";
+import { start, stop } from "../index";
+import { getRequest, postRequest } from "./utility";
+import { BackendErrorType } from "../src/BackendError";
+import { response } from "express";
+
+beforeAll(async () => {
+    await start();
+});
+
+afterAll(async () => {
+    await stop();
+});
+
+describe("createComment Function", () => {
+    it("Test API integration", async () => {
+        const response = await postRequest("/createComment", {
+            username: "alice123",
+            postId: "6629b52321a4e8bba1adc12a",
+            commentString: "This is a test comment"
+        });
+        expect(response).not.toEqual(undefined);
+        expect(response.result).toEqual("SUCCESS");
+    });
+
+    it("Test Helper Function", async () => {
+        const response = await createComment("alice123", "6629b52321a4e8bba1adc12a", "This is a test comment");
+        expect(response).not.toEqual(undefined);
+        expect(response.result).toEqual("SUCCESS");
+    });
+
+    it("Test Invalid User", async () => {
+        try {
+            await createComment("invalidUser", "6629b52321a4e8bba1adc12a", "This is a test comment");
+            throw new Error("Should Not Reach Here");
+        } catch (e) {
+            expect(e).toEqual(BackendErrorType.USER_DNE);
+        }
+    });
+
+    it("Test Invalid Post", async () => {
+        try {
+            await createComment("alice123", "6629b52321a4e8bba1adc13d", "This is a test comment");
+            throw new Error("Should Not Reach Here");
+        } catch (e) {
+            expect(e).toEqual(BackendErrorType.POST_DNE);
+        }
+    });
+
+    it("Test Comment Creation", async() => {
+        const response = await createComment("alice123", "6629b52321a4e8bba1adc12a", "This is a test comment");
+        // check the comments on that post
+        const comments = await getComments("6629b52321a4e8bba1adc12a");
+        expect(comments).not.toEqual(undefined);
+        expect(comments.length).toBeGreaterThan(0);
+
+        // check the comment content
+        const comment = comments[comments.length - 1];
+        expect(comment).toHaveProperty("username");
+        expect(comment).toHaveProperty("comment_string");
+        expect(comment).toHaveProperty("comment_like_list");
+        expect(comment).toHaveProperty("comment_dislike_list");
+        expect(comment).toHaveProperty("date_created");
+
+        // check the comment content
+        expect(comment.username).toEqual("alice123");
+        expect(comment.comment_string).toEqual("This is a test comment");
+    });
+
+    it("Should Properly Handle If Id cannot be converted to ObjectId (Slightly Wrong)", async () => {
+        try {
+            await createComment("alice123", "invalidId", "This is a test comment");
+            throw new Error("Should Not Reach Here");
+        } catch (e) {
+            expect(e).toEqual(BackendErrorType.POST_DNE);
+        }
+    });
+
+    it("Should Properly Handle If Id cannot be converted to ObjectId (Completely Wrong)", async () => {
+        try {
+            await createComment("alice123", "invalidId", "This is a test comment");
+            throw new Error("Should Not Reach Here");
+        } catch (e) {
+            expect(e).toEqual(BackendErrorType.POST_DNE);
+        }
+    });
+
+    it("Should Increase Comment Count", async () => {
+        const commentsBefore = await getComments("6629b52321a4e8bba1adc12a");
+        const response = await createComment("alice123", "6629b52321a4e8bba1adc12a", "This is a test comment");
+        const commentsAfter = await getComments("6629b52321a4e8bba1adc12a");
+        expect(commentsAfter.length).toEqual(commentsBefore.length + 1);
+    });
+
+
+});
+
+describe("getComments Function", () => {
+    it("Test API integration", async () => {
+        const response = await getRequest("/getComments", {
+            postId: "6629b52321a4e8bba1adc12a"
+        });
+        expect(response).not.toEqual(undefined);
+        expect(response.length).toBeGreaterThan(0);
+    });
+
+    it("Test Helper Function", async () => {
+        const response = await getComments("6629b52321a4e8bba1adc12a");
+        expect(response).not.toEqual(undefined);
+        expect(response.length).toBeGreaterThan(0);
+    });
+
+    it("Test Invalid Post", async () => {
+        try {
+            await getComments("6629b52321a4e8bba1adc13d");
+            throw new Error("Should Not Reach Here");
+        } catch (e) {
+            expect(e).toEqual(BackendErrorType.POST_DNE);
+        }
+    });
+
+    it("Test Valid Post", async () => {
+        const response = await getComments("6629b52321a4e8bba1adc12a");
+        expect(response).not.toEqual(undefined);
+        expect(response.length).toBeGreaterThan(0);
+    });
+
+    it("Test Empty Post", async () => {
+        const response = await getComments("66417aec7d63165e0edd397c");
+        expect(response).not.toEqual(undefined);
+        expect(response.length).toEqual(0);
+    });
+
+    it("Should Properly Handle If Id cannot be converted to ObjectId (Slightly Wrong)", async () => {
+        try {
+            await getComments("invalidId");
+            throw new Error("Should Not Reach Here");
+        } catch (e) {
+            expect(e).toEqual(BackendErrorType.POST_DNE);
+        }
+    });
+
+    it("Should Properly Handle If Id cannot be converted to ObjectId (Completely Wrong)", async () => {
+        try {
+            await getComments("invalidId");
+            throw new Error("Should Not Reach Here");
+        } catch (e) {
+            expect(e).toEqual(BackendErrorType.POST_DNE);
+        }
+    });
+
+    it("Comments Returned Should Have Proper Fields", async () => {
+        const response = await getComments("6629b52321a4e8bba1adc12a");
+        expect(response).not.toEqual(undefined);
+        expect(response.length).toBeGreaterThan(0);
+
+        const comment = response[0];
+        expect(comment).toHaveProperty("username");
+        expect(comment).toHaveProperty("comment_string");
+        expect(comment).toHaveProperty("comment_like_list");
+        expect(comment).toHaveProperty("comment_dislike_list");
+        expect(comment).toHaveProperty("date_created");
+    });
+
+});
